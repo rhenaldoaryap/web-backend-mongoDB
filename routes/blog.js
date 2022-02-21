11778a75766368631c8a471782e794ac7a487011
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-21 14:15:45 +0700

    success creating blog post with MongoDb and NodeJs

--- routes/blog.js
+++ routes/blog.js
@@ -1,17 +1,53 @@
-const express = require('express');
+const express = require("express");
+const mongoDb = require("mongodb");
+
+const db = require("../data/database");
+
+// ObjectId will be a class we can instantiate for looking an ID with mongoDB built-in
+const ObjectId = mongoDb.ObjectId;
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.redirect('/posts');
+router.get("/", function (req, res) {
+  res.redirect("/posts");
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get("/posts", function (req, res) {
+  res.render("posts-list");
 });
 
-router.get('/new-post', async function(req, res) {
-  res.render('create-post');
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  res.render("create-post", { authors: authors });
 });
+
+// Start create
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+
+  // get specific author
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  // Fetching data from form
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  console.log(result);
+  res.redirect("/posts");
+});
+// End of create
 
 module.exports = router;
