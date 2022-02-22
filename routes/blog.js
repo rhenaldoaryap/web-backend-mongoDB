461b4f117ee62f369375eb864c1031f08474c516
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-02-22 16:55:05 +0700

    adding the correct syntax for projection with MongoDB

--- routes/blog.js
+++ routes/blog.js
@@ -51,7 +51,10 @@ router.get("/posts", async function (req, res) {
   const posts = await db
     .getDb()
     .collection("posts")
-    .find({}, { title: 1, summary: 1, "author.name": 1 })
+    // correct syntax when add the "projection" to only fetch selected fields from database
+    .find({})
+    .project({ title: 1, summary: 1, "author.name": 1 })
+    // .find({}, { title: 1, summary: 1, "author.name": 1 })
     .toArray();
   res.render("posts-list", { posts: posts });
 });
